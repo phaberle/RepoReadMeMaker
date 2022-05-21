e43492709f77d770fd772a406722acab68c69418
Paul Haberle <61989020+phaberle@users.noreply.github.com>
2022-05-21 02:23:44 -0500

    tested several time. it works.

--- index.js
+++ index.js
@@ -5,40 +5,40 @@ const { Console } = require("console");
 const { cleanupAnswers, getLicenseTag, writeMDFile } = require('./tools.js');
 
 
-const UserInput = () => {
-    return inquirer.prompt([        
+const UserInput = async () => {
+    const UserInput = await inquirer.prompt([
         {
             type: 'input',
             name: 'gitHubName',
             message: "REQUIRED ==>What is your GitHub username?",
-            validate: gitHubName => {
-                if (gitHubName) {
+            validate: gitHubName_1 => {
+                if (gitHubName_1) {
                     return true;
                 } else {
                     console.log("Please enter your GitHub username.");
                     return false;
                 }
             }
-        },    
+        },
         {
             type: 'input',
             name: 'emailAddress',
             message: "REQUIRED ==>What is your email address?",
-            validate: emailInput => {
-                if (emailInput) {
+            validate: emailInput_1 => {
+                if (emailInput_1) {
                     return true;
                 } else {
                     console.log("Please enter your email address.");
                     return false;
                 }
             }
-        },  
+        },
         {
             type: 'editor',
             name: 'getInContact',
             message: 'REQUIRED ==> Enter a minimum of three lines on how contributors can contact you.',
-            validate: getInContact => {
-                if (getInContact.split('\n').length >= 3) {
+            validate: getInContact_1 => {
+                if (getInContact_1.split('\n').length >= 3) {
                     return true;
                 } else {
                     console.log("Please information on how you may be contacted.");
@@ -47,157 +47,113 @@ const UserInput = () => {
             }
         },
         {
-        type: 'input',
-        name: 'projectTitle',
-        message: "REQUIRED ==> What is the name of your project?",
-        validate: projectName => {
-            if (projectName) {
-                return true;
-            } else {
-                console.log("Please enter a name of your project");
-                return false;
+            type: 'input',
+            name: 'projectTitle',
+            message: "REQUIRED ==> What is the name of your project?",
+            validate: projectName_1 => {
+                if (projectName_1) {
+                    return true;
+                } else {
+                    console.log("Please enter a name of your project");
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'editor',
+            name: 'description',
+            message: 'REQUIRED ==> Enter a minimum of three lines describing your motivation or problems you solved.',
+            validate: descriptionInput_1 => {
+                if (descriptionInput_1.split('\n').length >= 3) {
+                    return true;
+                } else {
+                    console.log("Please enter a description of your project.");
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'editor',
+            name: 'install',
+            message: 'REQUIRED ==> Enter a minimum of three steps on how to install your project.',
+            validate: installInput_1 => {
+                if (installInput_1.split('\n').length >= 3) {
+                    return true;
+                } else {
+                    console.log("Please enter steps to install your project.");
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'editor',
+            name: 'usage',
+            message: "REQUIRED ==> Enter a minimum of three lines on how to use your project.\nCan enter relative path or image URLs using this matrix -> '![ALT Message]([relative/URL path])'",
+            validate: installInput_3 => {
+                if (installInput_3.split('\n').length >= 3) {
+                    return true;
+                } else {
+                    console.log("Please enter steps to install your project.");
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'checkbox',
+            name: 'license',
+            message: "REQUIRED => Choose your license (Pick One)...",
+            choices: ['Apache 2.0', 'MIT', 'GNU GPLv3'],
+            validate(license) {
+                if (license.length === 1) {
+                    return true;
+                } else {
+                    console.log("Pick only ONE license.");
+                    return false;
+                }
+            }
+        },
+        {
+            type: "editor",
+            name: "contribute",
+            message: "REQUIRED ==> How can others contribute to your project? List a minimum of three steps.",
+            validate: contribute_1 => {
+                if (contribute_1.split('\n').length >= 3) {
+                    return true;
+                } else {
+                    console.log("How can others contribute to your project?");
+                    return false;
+                }
+            }
+        },
+        {
+            type: "editor",
+            name: "tests",
+            message: "REQUIRED ==> List a minimum of three lines on how testing can be done.",
+            validate: testsInput_1 => {
+                if (testsInput_1.split('\n').length >= 3) {
+                    return true;
+                } else {
+                    console.log("How can you perform testing?");
+                    return false;
+                }
             }
         }
-    },
-    {
-        type: 'editor',
-        name: 'description',
-        message: 'REQUIRED ==> Enter a minimum of three lines describing your motivation or problems you solved.',
-        validate: descriptionInput => {
-            if (descriptionInput.split('\n').length >= 3) {
-                return true;
-            } else {
-                console.log("Please enter a description of your project.");
-                return false;
-            }
-        }
-    },
-    {
-        type: 'editor',
-        name: 'install',
-        message: 'REQUIRED ==> Enter a minimum of three steps on how to install your project.',
-        validate: installInput => {
-            if (installInput.split('\n').length >= 3) {
-                return true;
-            } else {
-                console.log("Please enter steps to install your project.");
-                return false;
-            }
-        }
-    },
-    {
-        type: 'editor',
-        name: 'usage',
-        message: "REQUIRED ==> Enter a minimum of three lines on how to use your project.\nCan enter relative path or image URLs using this matrix -> '![ALT Message]([relative/URL path])'",
-        validate: installInput => {
-            if (installInput.split('\n').length >= 3) {
-                return true;
-            } else {
-                console.log("Please enter steps to install your project.");
-                return false;
-            }
-        }
-    },
-    {
-        type: 'checkbox',
-        name: 'license',
-        message: "REQUIRED => Choose your license (Pick One)...",
-        choices: ['Apache 2.0', 'MIT', 'GNU GPLv3'],
-        validate(license) {
-            if (license.length === 1) {
-                return true;
-            } else {
-                console.log("Pick only ONE license.");
-                return false;
-            }
-        }
-    },
-    {
-        type: "editor",
-        name: "contribute",
-        message: "REQUIRED ==> How can others contribute to your project? List a minimum of three steps.",
-        validate: contribute => {
-            if (contribute.split('\n').length >= 3) {
-                return true;
-            } else {
-                console.log("How can others contribute to your project?");
-                return false;
-            }
-        }
-    },
-    {
-        type: "editor",
-        name: "tests",
-        message: "REQUIRED ==> List a minimum of three lines on how testing can be done.",
-        validate: testsInput => {
-            if (testsInput.split('\n').length >= 3) {
-                return true;
-            } else {
-                console.log("How can you perform testing?");
-                return false;
-            }
-        }
-    }
     ])
-        .then(UserInput => {
-            return UserInput;
-        })
+
+    return UserInput;
 };
 
 
-// UserInput()
-//     .then(UserInput => {
-//         UserInput.description= cleanupAnswers(UserInput.description);
-//         UserInput.install= cleanupAnswers(UserInput.install);
-//         UserInput.contribute=cleanupAnswers(UserInput.contribute);
-//         UserInput.tests=cleanupAnswers(UserInput.tests);
-//         UserInput.usage=cleanupAnswers(UserInput.usage);
-//         UserInput.getInContact=cleanupAnswers(UserInput.getInContact);
-//         let [license]=UserInput.license;
-//         UserInput.license=getLicenseTag(license);
-    
-//         writeMDFile(UserInput); 
-//     });
+UserInput()
+    .then(UserInput => {
+        UserInput.description= cleanupAnswers(UserInput.description);
+        UserInput.install= cleanupAnswers(UserInput.install);
+        UserInput.contribute=cleanupAnswers(UserInput.contribute);
+        UserInput.tests=cleanupAnswers(UserInput.tests);
+        UserInput.usage=cleanupAnswers(UserInput.usage);
+        UserInput.getInContact=cleanupAnswers(UserInput.getInContact);
+        let [license]=UserInput.license;
+        UserInput.license=getLicenseTag(license);    
+        writeMDFile(UserInput); 
+    });
 
-var testData = {    
-        gitHubName: 'Dr. Emmett Brown',
-        emailAddress: 'e.brown@gmail.com',
-        getInContact: 'Do not contact me.\r\n' +
-          'I will not answer email correspondence.\r\n' +
-          'I want cookies.',
-        projectTitle: 'DeLorean Time Machine',
-        description: 'I went back to the future.\r\n' +
-          'I saw flying cars.\r\n' +
-          'Hey Einstein! \r\n' +
-          'I did it!!\r\n' +
-          'Great Scott!',
-        install: 'Put on a radiation suit. \r\n' +
-          'Grab the stolen plutonium.\r\n' +
-          'Insert plutonium into the Flux Capacitor.\r\n' +
-          'Sit down in the DeLorean and set the time coordinates.\r\n' +
-          'Reach 88MPH.\r\n' +
-          'Hang on!',
-        usage: '![time circuits](https://static.wikia.nocookie.net/bttf/images/d/d5/Time_Circuits_BTTF.png/revision/latest?cb=20110823082816)\r\n' +
-          '![DeLorean](https://static.turbosquid.com/Preview/2020/03/17__01_57_35/DeLoreanDMC12TimeMachine3dmodel001.jpgF69C4732-35AD-4879-B3C8-7E72C7001DF3DefaultHQ.jpg)\r\n' +
-          'Do not use the time machine for profiteering. \r\n' +
-          'Try to be an observer only.',
-        license: [ 'MIT' ],
-        contribute: 'Not presently taking contributions.\r\n' +
-          'You would get in the way, no offense.\r\n' +
-          'The kind of work I do is not for amateur scientists.',
-        tests: 'Find a dog.\r\n' +
-          'Put dog in DeLorean with clock around its neck.\r\n' +
-          'Set time circuits to one minute in the future.\r\n' +
-          'Remote pilot DeLorean to 88MPH.\r\n' +
-          'Dog will come back in exactly one minute.\r\n' +
-          "Check dog's clock to current time. \r\n" +
-          'Will notice it is precisely one minute behind.'
-      }
-testData.description = cleanupAnswers(testData.description);
-testData.install = cleanupAnswers(testData.install);
-testData.contribute = cleanupAnswers(testData.contribute);
-testData.tests = cleanupAnswers(testData.tests);
-let[license]=testData.license;
-testData.license=getLicenseTag(license);
-testData.license = getLicenseTag(testData.license);
-writeMDFile(testData); 
--- tools.js
+++ tools.js
@@ -1,28 +1,92 @@
-const cleanupAnswers = (object) => {
-    let x = JSON.stringify(object);
-    const regex = /\\r\\n/g;
-    object = x.replace(regex, '<br>');
-    return object;
-  }
-  
-  const getLicenseTag = (string) => {
-    let x = string
-    switch (x) {
-      case  "Apache 2.0":
-        string = "[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)";
-        break;
-      case  "MIT":
-        string = "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)";
-        break;
-      case "GNU GPLv3":
-        string = "[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)";
-    }
-    object= [x,string];
-    return object;
-  }
+const fs = require('fs');
 
-const writeMDFile =(object)=>{
-console.log(object);
+const cleanupAnswers = (object) => {
+  let x = JSON.stringify(object);
+  const regex = /\\r\\n/g;
+  object = x.replace(regex, '<br>');
+  return object;
 }
 
-  module.exports={cleanupAnswers,getLicenseTag,writeMDFile};
+const getLicenseTag = (string) => {
+  let x = string
+  switch (x) {
+    case "Apache 2.0":
+      string = "[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)";
+      break;
+    case "MIT":
+      string = "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)";
+      break;
+    case "GNU GPLv3":
+      string = "[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)";
+  }
+  object = [x, string];
+  return object;
+}
+
+const writeMDFile = (object) => {
+  const { gitHubName, emailAddress, getInContact, projectTitle, description, install, usage, contribute, tests } = object;
+  const license = object.license[0];
+  const licURL = object.license[1];
+
+  let template =
+    `
+# ${projectTitle}
+
+## Description
+${description}
+
+## License
+${license}<br>
+${licURL}
+
+## Table of Contents
+* [Description](#description)
+* [License](#license)
+* [Installation](#installation)
+* [Usage](#usage)
+* [License](#license)
+
+## Installation
+${install}
+
+## Usage
+${usage}
+
+## Contribute
+${contribute}
+
+## Testing
+${tests}
+
+## Questions
+GitHub User Name: ${gitHubName}<br>
+Email Address: ${emailAddress}
+
+### How to reach me with additional questions:
+${getInContact}
+`;
+
+const regex = /"/g;
+const dir = "./dist";
+template = template.replace(regex, '');
+if(!fs.existsSync(dir)){fs.mkdirSync(dir)};
+ if(saveFile(template)){console.log("File successfully created.\nCheck the dist folder.");}
+}
+
+const saveFile=(template) => {
+  return new Promise((resolve, reject) => {
+    fs.writeFile('./dist/README.md', template, err => {
+      if (err) {
+        reject(err);
+        return;
+      }
+      resolve({
+        ok: true
+      });
+    });
+  });
+}
+
+
+module.exports = { cleanupAnswers, getLicenseTag, writeMDFile };
+
